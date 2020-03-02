1f78ac7e7f3a982504cf6ad60273731096129bb2
BardwellTK <Bardwell97@outlook.com>
2020-03-02 22:23:50 +0000

    Initial Commit

---
+++ core.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+/*User is placed in an office.
+User can traverse the office and interact with the scene and characters.
+The goal is to get the highest possible score.*/
+
+
+int main(int argc, char** argv)
+{
+    
+    
+    return 0;
+}
